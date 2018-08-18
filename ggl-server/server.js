54021de21a78bb00d36c3da92ddc0da47d0ede70
mochi8k <y.hkh.185@gmail.com>
2018-08-19 00:10:44 +0900

    add ggl-server

---
+++ ggl-server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// GraphQL schema
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+// Root resolver
+const resolver = {
+    message: () => 'Hello World!'
+};
+
+// Create an express server and a GraphQL endpoint
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: resolver,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('Express GraphQL Server Now Running On localhost:4000/graphql');
+});
